48d0adae40db4b7026cb0e9a7f34513101af17cc
CordeiroGente <pedrohpcordeiro@gmail.com>
2025-09-19 09:06:10 -0300

    Finalizado

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
+# python
 from stats import get_num_words, count_letters
+import sys
 
 def get_book_text(path):
-    with open(path, 'r', encoding='utf-8') as f:
-        file_content = f.read()
-    return file_content
+    with open(path) as f:
+        return f.read()
 
 def main():
-    path = './books/frankenstein.txt'
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path}...")
-    
+
     book_text = get_book_text(path)
     num_words = get_num_words(book_text)
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    
+
     letter_counts = count_letters(book_text)
     print("--------- Character Count -------")
-    # Ordena por valor decrescente
     for letter, count in sorted(letter_counts.items(), key=lambda item: item[1], reverse=True):
+        if not letter.isalpha():
+            continue
         print(f"{letter}: {count}")
     print("============= END ===============")
 
